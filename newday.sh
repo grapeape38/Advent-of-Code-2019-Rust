800e7588d6596ec8eb39664c1db93ff5464e91e4
grapeape38 <Eric.Russo@jmp.com>
2019-12-06 23:54:53 -0500

    changes

--- newday.sh
+++ newday.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 day=$(awk '/const MAX_DAYS/{print $5; }' src/main.rs | rev | cut -c 2 | rev)
 day=$(($day+1))
-sed -i -r "s/(const MAX_DAYS: usize = ).*;/\1$day;/g" src/main.rs 
 sed -i "$day i pub mod day$day;" src/days/mod.rs
 sed -i -r "s/(pub const day_fns: .*)];/\1, day$day::day$day];/g" src/days/mod.rs
 touch "input/day$day.in"
--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,11 @@ use std::fs::File;
 use days::day_fns;
 use clap::{App, Arg};
 
-const MAX_DAYS: usize = 6;
-
-fn main() -> Result<(), Box<dyn Error>> {
-     let matches = App::new("Advent of Code 2019")
-        .arg(Arg::with_name("run").short("r").takes_value(true))
-        .arg(Arg::with_name("test").short("t").takes_value(true))
-        .get_matches();
-
-    let day_num = matches.value_of("run").or(matches.value_of("test"))
-        .ok_or("Supply which day's challenge to run")?;
-    let day_num = str::parse::<usize>(day_num)?; 
-    if day_num > MAX_DAYS {
-        return Err("invalid day".into());
+fn run(path: &str, day_num: usize) -> Result<(), Box<dyn Error>> {
+    if day_num > day_fns.len() {
+        return Err("Invalid day".into());
     }
-    let folder = if matches.is_present("run") { "input" } else { "test" };
-    let mut f = File::open(&format!("{}/day{}.in", folder, day_num))
-        .map_err(|_| format!("File for day {:?} not found in inputs directory", day_num))?;
+    let mut f = File::open(path)?;
     let res = (day_fns[day_num - 1])(&mut f);
     match res {
         Ok(res) => {
@@ -33,4 +21,26 @@ fn main() -> Result<(), Box<dyn Error>> {
             Err(e)
         }
     }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+     let matches = App::new("Advent of Code 2019")
+        .arg(Arg::with_name("run").short("r").takes_value(true))
+        .arg(Arg::with_name("test").short("t").takes_value(true))
+        .get_matches();
+   
+    if let Some(day_num) = matches.value_of("run") {
+        let day_num = str::parse::<usize>(day_num)?; 
+        println!("Running day {:?}", day_num);
+        run(&format!("input/day{}.in", day_num), day_num)?;
+    }
+    if let Some(day_num) = matches.value_of("test") {
+        let day_num = str::parse::<usize>(day_num)?; 
+        println!("Testing day {:?}", day_num);
+        run(&format!("test/day{}.in", day_num), day_num)?;
+    }
+    if matches.is_present("run") || matches.is_present("test") {
+        Ok(())
+    }
+    else { Err("Pass a day with -r or -t to run/test".into()) }
 }
